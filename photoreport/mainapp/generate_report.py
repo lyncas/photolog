399904e3ff15202899a9da3353f51e573d44a9a8
Andrew Lynch <lyncas@gmail.com>
2016-11-07 17:35:53 -0600

    two photos per page, simple text

--- photoreport/mainapp/generate_report.py
+++ photoreport/mainapp/generate_report.py
@@ -10,22 +10,27 @@ class DocumentGenerator(object):
         document = Document()
 
         document.add_heading(project.name, 0)
-
+	img_idx = 1
         for image in project.sorted_images:
-            document.add_paragraph(image.caption, style='IntenseQuote')
+            img_txt = str(img_idx) + ". " + image.caption
+            document.add_paragraph(img_txt)
 
             img_full_url = os.path.join(
                 settings.MEDIA_ROOT, image.image.url.split('/media/')[1]
             )
             img_width = image.image.width
             if img_width > 500:
-                width = Inches(4)
+                width = Inches(4.97)
             else:
                 width = None
             if width:
                 document.add_picture(img_full_url, width=width)
             else:
                 document.add_picture(img_full_url)
-            document.add_page_break()
+            img_idx += 1
+
+            if img_idx % 2:
+                document.add_page_break()
+
         # document.save(file_name)
         return document
