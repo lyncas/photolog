fcb1db18671dab8746e0004dec63c7c88d59738c
Gonzalo Chacón Alípaz <gonzalo.chacon.alipaz@gmail.com>
2016-11-30 18:09:06 -0400

    Center images on the exported .docx file.

--- photoreport/mainapp/generate_report.py
+++ photoreport/mainapp/generate_report.py
@@ -1,7 +1,8 @@
 from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.shared import Pt
 from docx.shared import Inches
-#import docx.enum.style import WD_STYLE_TYPE 
+#import docx.enum.style import WD_STYLE_TYPE
 import os
 from django.conf import settings
 
@@ -11,14 +12,14 @@ class DocumentGenerator(object):
     def create(self, project):
         document = Document()
         obj_style = document.styles['Normal']
-	#obj_charstyle = obj_style.add_style('CaptionStyle', WD_STYLE_TYPE.CHARACTER)
-	obj_font = obj_style.font
-	obj_font.size = Pt(12)
-	obj_font.name = 'Tahoma'
-	#paragraph.style = document.styles['Normal']
+        #obj_charstyle = obj_style.add_style('CaptionStyle', WD_STYLE_TYPE.CHARACTER)
+        obj_font = obj_style.font
+        obj_font.size = Pt(12)
+        obj_font.name = 'Tahoma'
+        #paragraph.style = document.styles['Normal']
 
         #document.add_heading(project.name, 0)
-	img_idx = 1
+        img_idx = 1
         for image in project.sorted_images:
             img_txt = str(img_idx) + ". " + image.caption
             document.add_paragraph(img_txt, document.styles['Normal'])
@@ -26,14 +27,16 @@ class DocumentGenerator(object):
             img_full_url = os.path.join(
                 settings.MEDIA_ROOT, image.image.url.split('/media/')[1]
             )
-            #img_width = image.image.width  #todo if images are too small throw bug? 
+            #img_width = image.image.width  #todo if images are too small throw bug?
             document.add_picture(img_full_url, width=Inches(5.0))#5.25 inches is other size
-            #document.add_paragraph(" ")#add line break            
+            last_paragraph = document.paragraphs[-1]  # grab the last paragraph
+            last_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER  # center the content of the last paragraph
+            #document.add_paragraph(" ")#add line break
 
             img_idx += 1
             if img_idx % 2:#add two images and then page break
                 document.add_page_break()
             else:
-		document.add_paragraph(" ") #add line break 
+                document.add_paragraph(" ") #add line break
         # document.save(file_name)
         return document
