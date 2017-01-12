dcb8bebef7653e9e3bc83c63de625d51733b1938
Andrew Lynch <lyncas@gmail.com>
2017-01-12 13:00:34 -0600

    formatting changes to word document generation

--- photoreport/mainapp/generate_report.py
+++ photoreport/mainapp/generate_report.py
@@ -1,5 +1,6 @@
 from docx import Document
 from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.enum.text import WD_LINE_SPACING
 from docx.shared import Pt
 from docx.shared import Inches
 import os
@@ -19,15 +20,23 @@ class DocumentGenerator(object):
         for image in project.sorted_images:
             if len(image.caption) > 5: #no caption if less than 5 characters in caption
 	    	img_txt = str(img_idx) + ". " + image.caption
-            	document.add_paragraph(img_txt, document.styles['Normal'])
+            	p = document.add_paragraph(style ='Normal',text = img_txt)
+		p = document.paragraphs[-1]
+		#p.line_spacing = 1.0
+            	#p = document.add_paragraph(style ='ListBullet',text = img_txt)
+		#p.num_id = img_idx 
+		#p.level = 0
+		#p.line_spacing_rule = WD_LINE_SPACING.AT_LEAST
+		#p.line_spacing = 1.5
 		
+
             	img_full_url = os.path.join(
                 	settings.MEDIA_ROOT, image.image.url.split('/media/')[1]
             	)
             	#img_width = image.image.width  #todo if images are vertical handle differently
             	document.add_picture(img_full_url, width=Inches(4.8))
-            	last_paragraph = document.paragraphs[-1]  # grab the last paragraph
-            	last_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER  # center the content of the last paragraph
+            	p = document.paragraphs[-1]  # grab the last paragraph
+            	p.alignment = WD_ALIGN_PARAGRAPH.CENTER  # center the content of the last paragraph
             	img_idx += 1
             	if img_idx % 2:#add two images and then page break
                 	document.add_page_break()
