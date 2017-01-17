50138343abf33a52e85cce4555104f7d5226b789
Andrew Lynch <lyncas@gmail.com>
2017-01-17 12:24:56 -0600

    formatting on vertical images

--- photoreport/mainapp/generate_report.py
+++ photoreport/mainapp/generate_report.py
@@ -5,7 +5,7 @@ from docx.shared import Pt
 from docx.shared import Inches
 import os
 from django.conf import settings
-
+from PIL import Image
 
 class DocumentGenerator(object):
 
@@ -22,24 +22,31 @@ class DocumentGenerator(object):
 	    	img_txt = str(img_idx) + ". " + image.caption
             	p = document.add_paragraph(style ='Normal',text = img_txt)
 		p = document.paragraphs[-1]
-		#p.line_spacing = 1.0
+		p.line_spacing = 1.0
             	#p = document.add_paragraph(style ='ListBullet',text = img_txt)
 		#p.num_id = img_idx 
 		#p.level = 0
 		#p.line_spacing_rule = WD_LINE_SPACING.AT_LEAST
+		p.line_spacing_rule = WD_LINE_SPACING.EXACTLY
+	
 		#p.line_spacing = 1.5
 		
 
             	img_full_url = os.path.join(
                 	settings.MEDIA_ROOT, image.image.url.split('/media/')[1]
             	)
-            	#img_width = image.image.width  #todo if images are vertical handle differently
-            	document.add_picture(img_full_url, width=Inches(4.8))
-            	p = document.paragraphs[-1]  # grab the last paragraph
-            	p.alignment = WD_ALIGN_PARAGRAPH.CENTER  # center the content of the last paragraph
-            	img_idx += 1
+		im = Image.open(img_full_url)
+		(img_width, img_len) = im.size
+            	if img_width > img_len:   #wide picture
+			document.add_picture(img_full_url, width=Inches(4.3))
+            	else: 
+			document.add_picture(img_full_url, width=Inches(2.44))
+		
+		p = document.paragraphs[-1]  # grab the last paragraph
+            	p.alignment = WD_ALIGN_PARAGRAPH.CENTER  # center image
+		img_idx += 1
             	if img_idx % 2:#add two images and then page break
                 	document.add_page_break()
-            	else:
-                	document.add_paragraph(" ") #add line break
+            	#else:
+                #	document.add_paragraph(" ") #add line break
         return document
